bc32555f1158dbf7d447cdeda1f21ea88c5cf817
vlad <123vladblack123@gmail.com>
2022-12-21 16:23:45 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const answers= [];
+
+answers[0]= prompt("как ваше имя ?", "");
+answers[1]= prompt("как ваша фамилия ?", "");
+answers[2]= prompt("Сколько вам лет ?", "");
+
+document.write(answers);
+
+//const answer=+prompt("Вам есть 18", "18");
+//console.log(answer+ 5);
+
+//const result= confirm("Are you here ?");
+//console.log(result);
+
+
+//alert("Hello");
+
+
+//let arrA=["a", "b", "c"]
+//const arrB= {
+//    "a": "d",
+//   "1":"b",
+//   2:"s"
+//}
+//arrB.b="1234";
+
+//console.log(arrB["b"]);
+
+//let arr= ["g/jpg", {}, 8944 ]
+//console.log(arr[1]);
+//const obj = {
+//    name: "Jhon";
+//   age: 25;
+//    isMarried: false
+//};
+
+//console.log(obj.name)
+
+//let number = 5;
+//const lefBorderWidth = 1;
+
+//number=10;
+//console.log(number);
+
+//const obj = {
+ //   a:50
+//;
+
+//obj.a = 10;
+
+//console.log(obj);
