e85af657f0a7ac7534df507dbf64a19009a0e188
vlad <123vladblack123@gmail.com>
2022-12-23 13:38:02 +0200

    add css file

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
-const answers= [];
+"use strict"
 
-answers[0]= prompt("как ваше имя ?", "");
-answers[1]= prompt("как ваша фамилия ?", "");
-answers[2]= prompt("Сколько вам лет ?", "");
+console.log("arr" + `-object`);
+console.log(4 + "5");
 
-document.write(answers);
+let inkr=10,
+    decr=10;
+
+//    ++inkr;
+//    --decr;
+
+    console.log(++inkr);
+    console.log(--decr);
+
+    console.log(4%5);
+
+    console.log(2+2*2 !=8);
+    
+
+    const isChecked = false;
+          isClose=false;
+
+    console.log(isChecked || !isClose);
+    //console.log(isChecked && isClose);
+
+//const category="toys";
+
+//console.log(`https://someurl.com/${category}/5`);
+
+//const user = "Ivan";
+
+//alert(`Привет, ${user}`);
+
+
+//const answers= [];
+
+//answers[0]= prompt("как ваше имя ?", "");
+//answers[1]= prompt("как ваша фамилия ?", "");
+//answers[2]= prompt("Сколько вам лет ?", "");
+
+
+
+//console.log(typeof(answers));
+//console.log(typeof(null));
 
 //const answer=+prompt("Вам есть 18", "18");
 //console.log(answer+ 5);
